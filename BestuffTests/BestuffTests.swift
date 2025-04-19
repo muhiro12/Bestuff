caf11775f8746e9637f268e7e1f515987b49acfa
muhiro_ <muhiro.12@gmail.com>
2025-04-19 16:37:49 +0900

    Initial Commit

---
+++ BestuffTests/BestuffTests.swift
@@ -0,0 +1,17 @@
+//
+//  BestuffTests.swift
+//  BestuffTests
+//
+//  Created by Hiromu Nakano on 2025/04/19.
+//
+
+import Testing
+@testable import Bestuff
+
+struct BestuffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
