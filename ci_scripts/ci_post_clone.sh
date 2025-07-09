ce36abedd5e6fc513068f2d8562f6103d9bf4206
muhiro_ <muhiro.12@gmail.com>
2025-07-09 11:11:38 +0900

    Add ci_scripts

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Bestuff
+#
+#  Created by Hiromu Nakano on 2025/07/09.
+#  Copyright © 2025 Hiromu Nakano. All rights reserved.
+
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
