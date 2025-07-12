c9e15ac0ea01741210e64043ed209efc194644e8
muhiro_ <58671793+muhiro12@users.noreply.github.com>
2025-07-12 20:18:52 +0900

    Replace HomeTabBar with HomeNavigationView

---
+++ Bestuff/Sources/Home/Views/HomeNavigationView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct HomeNavigationView: View {
+    var body: some View {
+        StuffNavigationView()
+    }
+}
+
+#Preview(traits: .sampleData) {
+    HomeNavigationView()
+}
